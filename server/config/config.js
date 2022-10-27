6d0ac4fb0eed0aa51ccf26b9e6083aaf6291066d
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-10-27 19:20:08 +0300

    Added the Mongo URI to config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,3 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+// export const MongoURI = "mongodb://localhost:27017/media";
 export const Secret = "someSecret";
+export const MongoURI = "mongodb+srv://Salma:salma123@comp229.wjzgekh.mongodb.net/media3?retryWrites=true&w=majority";
