bd175392bbd6e8afb0300e10377298f8cd57a22a
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-10-27 19:57:13 +0300

    Fixed the add button

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -19,6 +19,7 @@ export function displayAddPage(req, res, next) {
     /*****************
     * ADD CODE HERE *
     *****************/
+   res.render('index', { title: 'Add Book', page: 'books/edit', book: {} });
 }
 
 // POST process the Book Details page and create a new Book - CREATE
@@ -27,6 +28,22 @@ export function processAddPage(req, res, next) {
     /*****************
      * ADD CODE HERE *
      *****************/
+    let newBook = booksModel({
+        name: req.body.name,
+        author: req.body.author,
+        published: req.body.published,
+        description: req.body.description,
+        price: req.body.price
+    });
+
+    booksModel.create(newBook, (err, Book) => {
+        if(err) {
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/books/list')
+    })
 }
 
 // GET the Book Details page in order to edit an existing Book
